f625663e1bbffba87aaff16522daf13883d193cb
Franek Budrowski <fbudrowski@gmail.com>
2021-01-30 23:18:14 +0000

    Added loading and saving models

--- stable_baselines/ppg/run_atari.py
+++ stable_baselines/ppg/run_atari.py
@@ -3,10 +3,13 @@ from stable_baselines.common.cmd_util import make_atari_env, atari_arg_parser
 from stable_baselines.common.vec_env import VecFrameStack
 from stable_baselines.common.policies import CnnPolicy, CnnLstmPolicy, CnnLnLstmPolicy, MlpPolicy
 from stable_baselines.ppg.ppg import PPGCnnPolicy, PPGMlpPolicy
+from datetime import datetime, timezone
+from pathlib import Path
 
 
-def train(env_id, num_timesteps, seed, policy,
-          n_envs=8, nminibatches=4, n_steps=128, logdir="/tmp/.tensorboard_logs/atari_ppg"):
+
+def train(env_id, num_timesteps, seed, policy, load_addr=None, save_addr="ppo2_model",
+          n_envs=8, nminibatches=4, n_steps=128, logdir="/tmp/.tensorboard_logs/atari_ppg", timesteps_per_save=None):
     """
     Train PPG model for atari environment, for testing purposes
 
@@ -23,13 +26,29 @@ def train(env_id, num_timesteps, seed, policy,
 
     env = VecFrameStack(make_atari_env(env_id, n_envs, seed), 4)
     policy = {'cnn': PPGCnnPolicy, 'mlp': PPGMlpPolicy}[policy]
-    model = PPG(policy=policy, env=env, n_steps=n_steps, nminibatches=nminibatches,
-                 lam=0.95, gamma=0.99, noptepochs=4, ent_coef=.01,
-                 learning_rate=lambda f: f * 2.5e-4, cliprange=lambda f: f * 0.1, verbose=1,
-                 full_tensorboard_log=logdir,
-                tensorboard_log=logdir)
-    print("Model created")
-    model.learn(total_timesteps=num_timesteps)
+    if load_addr:
+        model = PPG.load(load_addr)
+    else:
+        model = PPG(policy=policy, env=env, n_steps=n_steps, nminibatches=nminibatches,
+                     lam=0.95, gamma=0.99, noptepochs=4, ent_coef=.01,
+                     learning_rate=lambda f: f * 2.5e-4, cliprange=lambda f: f * 0.1, verbose=1,
+                     full_tensorboard_log=logdir,
+                    tensorboard_log=logdir)
+    # model.learn(total_timesteps=num_timesteps)
+
+    timesteps_per_save = timesteps_per_save or (num_timesteps // 2)
+    total_rounds = num_timesteps // timesteps_per_save
+    elapsed_timesteps = 0
+    start_time = f'{datetime.now(timezone.utc).strftime("%Y-%m-%d--%H-%M-%S-%Z")}'
+
+    for i in range(total_rounds + 1):
+        timesteps_to_do = min(timesteps_per_save, num_timesteps - elapsed_timesteps)
+        if timesteps_to_do > 0:
+            model.learn(total_timesteps=timesteps_to_do, reset_num_timesteps=False)
+            elapsed_timesteps += timesteps_to_do
+            pathdir = f"{save_addr}_{start_time}_{elapsed_timesteps}"
+            Path(pathdir).mkdir(parents=True, exist_ok=True)
+            model.save(pathdir)
 
     env.close()
     # Free memory
@@ -43,10 +62,13 @@ def main():
     parser = atari_arg_parser()
     parser.add_argument('--policy', help='Policy architecture', choices=['cnn', 'lstm', 'lnlstm', 'mlp'], default='cnn')
     parser.add_argument('--logdir', help='Directory to save Tensorboard logs', default=None)
+    parser.add_argument('--load_addr', help='Directory to load model', default=None)
+    parser.add_argument('--save_addr', help='Directory to save model', default="ppg_breakout")
+    parser.add_argument('--timesteps_per_save', help='', type=int, default=None)
     args = parser.parse_args()
     logger.configure()
-    train(args.env, num_timesteps=args.num_timesteps, seed=args.seed,
-          policy=args.policy, logdir=args.logdir)
+    train(args.env, num_timesteps=args.num_timesteps, seed=args.seed, load_addr=args.load_addr,
+          save_addr=args.save_addr, policy=args.policy, logdir=args.logdir, timesteps_per_save=args.timesteps_per_save)
 
 
 if __name__ == '__main__':
